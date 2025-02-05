9b41aa6b04c067a5ef43549d98ff991e9ed8de9c
bhig-sheydi <bhigsheydi@gmail.com>
2025-02-04 17:51:25 -0800

    isArmstrong fix7

--- api/utils.js
+++ api/utils.js
@@ -1,7 +1,7 @@
 function isPrime(num) {
     if (num < 2) return false;
     if (num === 2 || num === 3) return true;
-    if ((num & 1) === 0 || num % 3 === 0) return false; // Faster even check
+    if ((num & 1) === 0 || num % 3 === 0) return false; 
     for (let i = 5; i * i <= num; i += 6) {
         if (num % i === 0 || num % (i + 2) === 0) return false;
     }
@@ -10,20 +10,20 @@ function isPrime(num) {
 
 function isArmstrong(num) {
     let originalNum = num;
-    num = Math.abs(num); // Work with positive digits
+    num = Math.abs(num); 
     let sum = 0, temp = num, power = 0;
 
-    // Fast digit count without log
+    
     while (temp > 0) {
         power++;
-        temp = (temp / 10) | 0; // Faster than Math.floor
+        temp = (temp / 10) | 0; 
     }
 
     temp = num;
     while (temp > 0) {
         let digit = temp % 10;
         let powered = 1;
-        for (let i = 0; i < power; i++) powered *= digit; // Faster than Math.pow()
+        for (let i = 0; i < power; i++) powered *= digit; 
         sum += powered;
         temp = (temp / 10) | 0;
     }
@@ -33,16 +33,16 @@ function isArmstrong(num) {
 
 function getDigitSum(num) {
     let sum = 0;
-    num = Math.abs(num); // Ensure it's positive
+    num = Math.abs(num);
     while (num > 0) {
         sum += num % 10;
-        num = (num / 10) | 0; // Faster than Math.floor
+        num = (num / 10) | 0; 
     }
     return sum;
 }
 
 function isPerfect(num) {
-    if (num < 2 || (num & 1) === 1) return false; // No odd perfect numbers known
+    if (num < 2 || (num & 1) === 1) return false; 
     let sum = 1, sqrtNum = Math.sqrt(num);
     
     for (let i = 2; i <= sqrtNum; i++) {
@@ -56,7 +56,7 @@ function isPerfect(num) {
 function getProperties(num) {
     const properties = [];
     if (isArmstrong(num)) properties.push("armstrong");
-    properties.push((num & 1) === 0 ? "even" : "odd"); // Faster than modulus
+    properties.push((num & 1) === 0 ? "even" : "odd"); 
     if (isPerfect(num)) properties.push("perfect");
     return properties;
 }
